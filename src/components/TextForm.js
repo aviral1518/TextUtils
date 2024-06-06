f4245944a1fcef9eee32190f0e3bbc6a07e17ef7
Aviral Khandelwal <aviralkhandelwal15@gmail.com>
2024-06-06 11:30:13 +0530

    bugfix

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -83,11 +83,11 @@ export default function TextForm(props) {
       >
         <h2>Your text summary</h2>
         <p>
-          {text?.split(" ")?.filter((i) => i?.length !== 0)?.length} words and{" "}
+          {text?.split(/\s+/)?.filter((i) => i?.length !== 0)?.length} words and{" "}
           {text?.length} characters
         </p>
         <p>
-          {0.008 * text?.split(" ")?.filter((i) => i?.length !== 0)?.length}{" "}
+          {0.008 * text?.split(/\s+/)?.filter((i) => i?.length !== 0)?.length}{" "}
           Minutes read
         </p>
         <h2>Preview</h2>
